80666e90a725a8f1c14050c66af456813f878214
Siddhesh Gajare <ssgajare12345@gmail.com>
2025-09-24 22:47:32 +0530

    Function Overloading.cpp

---
+++ Function Overloading.cpp
@@ -0,0 +1,32 @@
+// Exo - 12
+// Siddhesh
+// 25070123505 (A3)
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void calc(int a,int b){
+    int sum=a+b;
+    cout<< sum<<endl;
+}
+
+void calc(int a,int b,int c)
+{
+    int diffrence=a-b-c;
+    cout<< diffrence;
+}
+
+int main()
+{
+    
+     calc(15,5);
+     calc(5,10,20);
+    
+}
+
+Output :-
+20
+-25
+
+=== Code Execution Successful ===
